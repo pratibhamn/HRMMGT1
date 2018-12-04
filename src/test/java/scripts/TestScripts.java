13bb5c1dbe962843ad9018c6aa6e03a183bc6c83
Administrator <Administrator@Tyss-4F-Wacom>
2018-12-04 08:59:09 +0530

    2nd commit

--- src/test/java/scripts/TestScripts.java
+++ src/test/java/scripts/TestScripts.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import org.apache.poi.EncryptedDocumentException;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.openqa.selenium.By;
+import org.testng.Reporter;
 import org.testng.annotations.Test;
 
 import generic.BasePage;
@@ -27,6 +28,7 @@ public class TestScripts  extends BaseTest{
 		pi.setLastName("Fernandes");
 		pi.clickSaveBtn();
 		pi.verifyFirstName("Lilly");
+		Reporter.log("Hello",true);
 		
 	}
 	@Test(enabled=true)
